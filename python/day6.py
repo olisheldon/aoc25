33f87831beab53f75542f3cf0bb2d9417b28fbe7
Oliver Sheldon <oliversheldon00@gmail.com>
2025-12-06 20:29:56 +0000

    day 6 part 2

--- python/day6.py
+++ python/day6.py
@@ -2,27 +2,52 @@ import sys
 from pathlib import Path
 
 
-def part1(numbers: list[list[int]], operations: list[str]) -> int:
+def accumulate(col: list[int], op: str) -> int:
+    col_tot = 1 if op == "*" else 0
+    for num in col:
+        if op == "*":
+            col_tot *= num
+        else:
+            col_tot += num
+    return col_tot
+
+
+def part1(data: list[str]) -> int:
+    *numbers, operations = data
+    numbers = [list(map(int, row.split())) for row in numbers]
+    operations = operations.split()
     res = 0
     for col, op in zip(zip(*numbers), operations):
-        col_tot = 1 if op == "*" else 0
-        for num in col:
-            if op == "*":
-                col_tot *= num
-            else:
-                col_tot += num
-        res += col_tot
+        res += accumulate(col, op)
     return res
 
 
-def part2():
-    pass
+def part2(data: list[str]):
+    res = []
+    i = 0
+    col_wise = list(zip(*data))
+    op = col_wise[0][-1]
+    group = []
+    while i in range(len(col_wise)):
+        col = col_wise[i]
+        if all(x == " " for x in col):
+            res.append(accumulate(group, op))
+            group = []
+            i += 1
+            if i not in range(len(col_wise)):
+                break
+            op = col_wise[i][-1]
+            continue
+        *num, _ = col
+        num = int("".join(n for n in num if n))
+        group.append(num)
+        i += 1
+    res.append(accumulate(group, op))
+    return sum(res)
 
 
 def parse(data: str):
-    *numbers, operations = data.split("\n")
-    numbers = [list(map(int, row.split())) for row in numbers]
-    return numbers, operations.split()
+    return data.split("\n")
 
 
 if __name__ == "__main__":
@@ -31,5 +56,5 @@ if __name__ == "__main__":
         inp = f.read()
     inp_data = parse(inp)
 
-    print("part1=" + str(part1(*inp_data)))
+    print("part1=" + str(part1(inp_data)))
     print("part2=" + str(part2(inp_data)))
