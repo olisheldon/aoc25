422bbc8a7fabf7b671faca55d74e0d1e5e66c9e7
Oliver Sheldon <oliversheldon00@gmail.com>
2025-12-14 14:11:55 +0000

    day 12

--- python/day12.py
+++ python/day12.py
@@ -17,19 +17,22 @@ class Region:
     required_shapes: list[int]
 
 
-def part1():
-    # we want to minimize void space
-    # we do dfs but priority queue or something like that
-    # place blocks that minimize void space
-
-    # void space 
+def part1(regions: list[Region]) -> int:
+    # assume each shape is simply 3x3
+    res = 0
+    for region in regions:
+        num_of_presents = sum(region.required_shapes)
+        r, c = region.r, region.c
+        fits = (r // 3) * (c // 3) >= num_of_presents
+        res += fits
+    return res
 
 
 def part2():
     pass
 
 
-def parse(data: str):
+def parse(data: str) -> list[Region]:
     *shapes, regions = data.split("\n\n")
     shape_by_index: dict[int, list[list[Tiles]]] = defaultdict(list)
     for i, shape in enumerate(shapes):
@@ -40,6 +43,7 @@ def parse(data: str):
         dimensions, requirements = region.split(": ")
         r, c = list(map(int, dimensions.split("x")))
         required.append(Region(r, c, shape_by_index, list(map(int, requirements.split()))))
+    return required
 
 if __name__ == "__main__":
     filename = sys.argv[1] if len(sys.argv) > 1 else f"../data/{Path(__file__).stem}.txt"
