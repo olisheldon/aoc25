6c02ed99956c413b387e11d99c7defa496bda5be
Oliver Sheldon <oliversheldon00@gmail.com>
2025-12-20 16:22:00 +0000

    day 9 part 2 not working

--- python/day9.py
+++ python/day9.py
@@ -1,15 +1,20 @@
-from collections import defaultdict
+from collections import defaultdict, deque
+from functools import cache
 import heapq
 import sys
 from pathlib import Path
 from enum import StrEnum
 import bisect
 
+sys.setrecursionlimit(10000000)
+
 
 class Tile(StrEnum):
     RED = '#'
     GREEN = 'X'
     PLAIN = '.'
+    OUTSIDE = 'O'
+    INSIDE = 'I'
 
 
 def max_areas(tiles: list[tuple[int, int]]) -> list[tuple[int, tuple[int, int], tuple[int, int]]]:
@@ -27,23 +32,23 @@ def part1(tiles: list[tuple[int, int]]) -> int:
     return -max_areas(tiles)[0][0]
 
 
-def get_boundary_points(corner1: tuple[int, int], corner2: tuple[int, int]) -> list[tuple[int, int]]:
+def get_perimeter_points(corner1: tuple[int, int], corner2: tuple[int, int]) -> list[tuple[int, int]]:
     x1, y1 = corner1
     x2, y2 = corner2
     
-    coordinates: list[tuple[int, int]] = []
+    perimeter_points: list[tuple[int, int]] = []
     
     if x1 != x2:
         step = 1 if x2 > x1 else -1
         for x in range(x1, x2 + step, step):
-            coordinates.append((x, y1))
+            perimeter_points.append((x, y1))
     
     if y1 != y2:
         step = 1 if y2 > y1 else -1
         for y in range(y1 + step, y2 + step, step):
-            coordinates.append((x2, y))
+            perimeter_points.append((x2, y))
     
-    return coordinates
+    return perimeter_points
 
 
 def create_boundary(red_tiles: list[tuple[int, int]]) -> tuple[set[tuple[int, int]], defaultdict[int, list[int]], defaultdict[int, list[int]]]:
@@ -96,7 +101,7 @@ def print_boundary(boundary: defaultdict[int, list[int]], red_tiles: list[tuple[
 
 
 def is_valid_area(boundary_points: set[tuple[int, int]], boundary_r: defaultdict[int, list[int]], boundary_c: defaultdict[int, list[int]], points_i: tuple[int, int], points_j: tuple[int, int]) -> bool:
-    square_boundary = get_boundary_points(points_i, points_j)
+    square_boundary = get_perimeter_points(points_i, points_j)
     for r, c in square_boundary:
         if (r, c) in boundary_points:
             continue
@@ -107,16 +112,62 @@ def is_valid_area(boundary_points: set[tuple[int, int]], boundary_r: defaultdict
     return True
 
 
-def part2(red_tiles: list[tuple[int, int]]):
-    boundary_points, boundary_r, boundary_c = create_boundary(red_tiles)
-    # print_boundary(boundary_r, red_tiles)
+def compressed(red_tiles: list[tuple[int, int]]) -> int:
+    r_coords, c_coords = zip(*red_tiles)
+    ROWS, COLS = len(r_coords), len(c_coords)
+    NEIGHBOURS = ((1, 0), (-1, 0), (0, 1), (0, -1))
+    compressed_to_coord_r = {i : r for i, r in enumerate(r_coords)}
+    compressed_to_coord_c = {i : c for i, c in enumerate(c_coords)}
+    coord_to_compressed_r = {r : i for i, r in enumerate(r_coords)}
+    coord_to_compressed_c = {c : i for i, c in enumerate(c_coords)}
+    compressed_tiles: list[tuple[int, int]] = [(coord_to_compressed_r[r], coord_to_compressed_c[c]) for (r, c) in red_tiles]
+    compressed_grid = [[Tile.INSIDE for _ in range(ROWS)] for _ in range(COLS)]
+    boundary, boundary_r, _ = create_boundary(compressed_tiles)
+    for r in boundary_r:
+        for c in boundary_r[r]:
+            if (r, c) in compressed_tiles:
+                compressed_grid[r][c] = Tile.RED
+            else:
+                compressed_grid[r][c] = Tile.GREEN
+    
+    q = deque([(0, 0)])
+    visited: set[tuple[int, int]] = set()
+    while q:
+        for _ in range(len(q)):
+            r, c = q.popleft()
+            if (
+                r not in range(ROWS) or
+                c not in range(COLS) or
+                compressed_grid[r][c] is not Tile.INSIDE or
+                (r, c) in visited
+            ):
+                continue
+            visited.add((r, c))
+            compressed_grid[r][c] = Tile.OUTSIDE
+            
+            for dr, dc in NEIGHBOURS:
+                q.append((r + dr, c + dc))
+    
+    for row in compressed_grid:
+        print("".join(row))
+    
     max_areas_heap = max_areas(red_tiles)
     while max_areas_heap:
-        max_area, point_i, point_j = heapq.heappop(max_areas_heap)
-        if not is_valid_area(boundary_points, boundary_r, boundary_c, point_i, point_j):
+        area, (i_r, i_c), (j_r, j_c) = heapq.heappop(max_areas_heap)
+        compressed_i_coord = coord_to_compressed_r[i_r], coord_to_compressed_c[i_c]
+        compressed_j_coord = coord_to_compressed_r[j_r], coord_to_compressed_c[j_c]
+        perimeter_points = get_perimeter_points(compressed_i_coord, compressed_j_coord)
+        for r, c in perimeter_points:
+            if compressed_grid[r][c] is Tile.OUTSIDE:
+                break
+        else:
             continue
-        return -max_area
+        return -area
     return -1
+
+
+def part2(red_tiles: list[tuple[int, int]]):
+    return compressed(red_tiles)
 
 
 def parse(data: str) -> list[tuple[int, int]]:
